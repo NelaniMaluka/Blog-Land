b1577b8015e4241d09ec0395ccf6fc8b8ed9c121
Nelani Maluka <malukanelani@gmail.com>
2025-07-18 05:01:19 +0200

    initial setup

---
+++ blog-land-backend/src/main/java/com/nelani/blog_land_backend/BlogLandBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nelani.blog_land_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogLandBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogLandBackendApplication.class, args);
+	}
+
+}
---
+++ blog-land-backend/src/test/java/com/nelani/blog_land_backend/BlogLandBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nelani.blog_land_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogLandBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
