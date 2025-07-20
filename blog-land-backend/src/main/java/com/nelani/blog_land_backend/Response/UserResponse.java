011b8c3a7308ab3bc22c618563730ff8cfb33d33
Nelani Maluka <malukanelani@gmail.com>
2025-07-20 13:10:20 +0200

    added user response

---
+++ blog-land-backend/src/main/java/com/nelani/blog_land_backend/Response/UserResponse.java
@@ -0,0 +1,37 @@
+package com.nelani.blog_land_backend.Response;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class UserResponse {
+
+    @JsonProperty("id")
+    private Long id;
+
+    @JsonProperty("email")
+    private String email;
+
+    @JsonProperty("firstname")
+    private String firstname;
+
+    @JsonProperty("lastname")
+    private String lastname;
+
+    @JsonProperty("profileIconUrl")
+    private String profileIconUrl;
+
+    @JsonProperty("location")
+    private String location;
+
+    @JsonProperty("jwtToken")
+    private String jwtToken;
+}
