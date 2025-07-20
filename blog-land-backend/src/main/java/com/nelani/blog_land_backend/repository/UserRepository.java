85de95bd8b45b95d445a0568418f913cca2e6977
Nelani Maluka <malukanelani@gmail.com>
2025-07-20 13:09:23 +0200

    removed un-used imports

--- blog-land-backend/src/main/java/com/nelani/blog_land_backend/repository/UserRepository.java
+++ blog-land-backend/src/main/java/com/nelani/blog_land_backend/repository/UserRepository.java
@@ -2,9 +2,7 @@ package com.nelani.blog_land_backend.repository;
 
 import com.nelani.blog_land_backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.Optional;
-
 
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
