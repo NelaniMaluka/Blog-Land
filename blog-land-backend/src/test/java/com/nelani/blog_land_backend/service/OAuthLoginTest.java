5fb6047015e2c90fcf039b98162a1597420891a9
Nelani Maluka <malukanelani@gmail.com>
2025-08-16 18:04:45 +0200

    a

--- blog-land-backend/src/test/java/com/nelani/blog_land_backend/service/OAuthLoginTest.java
+++ blog-land-backend/src/test/java/com/nelani/blog_land_backend/service/OAuthLoginTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.nelani.blog_land_backend.Util.Validation.ModerationValidator;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -16,6 +19,12 @@ class OAuthLoginTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private ModerationValidator moderationValidator;
+
+    @MockBean
+    private ModerationClient moderationClient;
 
     @Test
     void whenUnauthenticated_thenRedirectToOAuthLogin() throws Exception {
@@ -30,4 +39,3 @@ class OAuthLoginTest {
                 .andExpect(status().isOk());
     }
 }
-
