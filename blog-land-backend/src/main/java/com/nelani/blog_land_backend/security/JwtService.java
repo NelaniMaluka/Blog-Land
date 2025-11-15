cd7540d19dea48e097bc5aeb6b8d0476c5ddbd38
Nelani Maluka <malukanelani@gmail.com>
2025-11-15 16:01:34 +0200

    .

--- blog-land-backend/src/main/java/com/nelani/blog_land_backend/security/JwtService.java
+++ blog-land-backend/src/main/java/com/nelani/blog_land_backend/security/JwtService.java
@@ -16,7 +16,7 @@ import java.util.function.Function;
 
 @Service
 public class JwtService {
-    @Value("${security.jwt.secret-key}")
+    @Value("${JWT_SECRET_KEY}")
     private String secretKey;
 
     @Value("${security.jwt.expiration-time}")
